b68704355985459e2390f356ae864cf9e88cf215
sammurphyportfolio <samuel.m1338@gmail.com>
2022-04-02 15:19:12 +1100

    First port of Figma design

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
+import DashboardTiles from '../components/DashboardTiles/DashboardTiles'
 import Header from '../components/Header/Header'
+import BottomNavBar from '../components/Nav/BottomNavBar'
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
       </Head>
       <div>
         <Header />
+        <DashboardTiles />
+        <BottomNavBar />
       </div>
     </>
     
